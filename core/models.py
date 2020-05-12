d7436a579a172f0b5c5b9c548ea9063aed017835
Shrawan907 <shrawan07090@gmail.com>
2020-05-12 17:22:34 +0530

    function based view

--- core/models.py
+++ core/models.py
@@ -1,12 +1,29 @@
 from django.conf import settings
 from django.db import models
 
+
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport Wear'),
+    ('OW', 'Outwear')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
+
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
 
     def _str_(self):
         return self.title
+
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
@@ -14,8 +31,10 @@ class OrderItem(models.Model):
     def _str_(self):
         return self.title
 
+
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     items = models.ManyToManyField(OrderItem)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from .views import item_list
+from .views import (
+    products,
+    checkout,
+    home
+)
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list')
+    path('', home, name='home'),
+    #path('', HomeView.as_view(), name='home'),
+    # have to call as_view() function so as to return a callable view that takes a request and returns a response.
+    # as_view() - Its the main entry-point in request-response cycle in case of generic views
+    path('checkout/', checkout, name="checkout"),
+    path('products/', products, name="products")
 ]
